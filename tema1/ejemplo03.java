397b5e3719b8b70eaddcbd94d9c369b2f96c8356
jpritin <jiprieto66@gmail.com>
2026-09-21 19:54:47 +0200

    Delete tema1/ejemplo03.java

--- tema1/ejemplo03.java
+++
@@ -1,9 +0,0 @@
-public class ejemplo03 {
-    public static void main(String[] args) {
-        System.out.println("Artículo    Precio  Nº de cajas");
-        System.out.println("==============================");
-        System.out.printf("%-10s %8.2f %6d\n", "Manzanas", 4.5, 10);
-        System.out.printf("%-10s %8.2f %6d\n", "Peras", 2.75, 120);
-        System.out.printf("%-10s %8.2f %6d\n", "Aguacates", 10.0, 6);
-    }
-}
